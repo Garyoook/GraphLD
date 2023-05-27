516d804f59490ac06072aac24132550f4560c417
Gary Gao <yg9418@ic.ac.uk>
2023-05-27 18:32:21 +0100

    refactor: simplify the structure of Dashboard code

---
+++ src/pages/Repositories/index.tsx
@@ -0,0 +1,225 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { DatabaseState } from '../reducer/databaseReducer';
+import { getRepoList } from '../service';
+
+function Repositories() {
+  const repo_graphDB = useSelector(
+    (state: DatabaseState) => state.database.repo,
+  );
+  const db_prefix_URL = useSelector(
+    (state: DatabaseState) => state.database.db_prefix_URL,
+  );
+
+  const dispatch = useDispatch();
+
+  const [selectRepoReminder, setSelectRepoReminder] = useState<boolean>(false);
+  const [columnsRepoTable, setColumnsRepoTable] = useState<any>([]);
+  const [dataRepoTable, setDataRepoTable] = useState<any>([]);
+  const [repoList, setRepoList] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSelectRepoReminder(
+      repo_graphDB === undefined || repo_graphDB === 'None',
+    );
+  }, [repo_graphDB]);
+
+  useEffect(() => {
+    fetchRepos();
+  }, []);
+
+  async function fetchRepos() {
+    try {
+      const response = await getRepoList();
+
+      if (response.status === 200) {
+        const data = response.data;
+        const data_bindings = data.results.bindings;
+
+        const columns = [
+          {
+            field: 'title',
+            headerName: 'Title',
+            width: 200,
+          },
+          {
+            field: 'uri',
+            headerName: 'URI',
+            width: 500,
+          },
+          {
+            field: 'writable',
+            headerName: 'Writable',
+            width: 200,
+          },
+          {
+            field: 'readable',
+            headerName: 'Readable',
+            width: 200,
+          },
+        ];
+
+        const dataSource = data_bindings.map(
+          (data_binding: any, index: number) => {
+            return {
+              id: index,
+              title: data_binding.id.value,
+              uri: data_binding.uri.value,
+              writable: data_binding.writable.value,
+              readable: data_binding.readable.value,
+            };
+          },
+        );
+
+        const repoList = data_bindings.map(
+          (data_binding: any, index: number) => {
+            return data_binding.id.value;
+          },
+        );
+
+        setRepoList(['None', ...repoList]);
+        setColumnsRepoTable(columns);
+        setDataRepoTable(dataSource);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const selectRepo = () => {
+    return (
+      <Grid>
+        Select a data repository:
+        <Select
+          size="small"
+          value={repo_graphDB ? repoList.indexOf(repo_graphDB) : 0}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+          style={{
+            margin: 5,
+            color:
+              repo_graphDB && repo_graphDB !== 'None' ? '#22cc22' : '#cc2222',
+            fontWeight: 'bold',
+          }}
+        >
+          {repoList.map((repo: any) => {
+            return (
+              <MenuItem
+                key={repoList.indexOf(repo)}
+                value={repoList.indexOf(repo)}
+                onClick={() => {
+                  dispatch({ type: 'database/setRepo', payload: repo });
+                }}
+                style={{
+                  color: repo !== 'None' ? '#22cc22' : '#cc2222',
+                  fontWeight: 'bold',
+                  borderRadius: 5,
+                  border: '1px solid #aaa',
+                  margin: 3,
+                }}
+              >
+                {repo}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </Grid>
+    );
+  };
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Paper sx={{ padding: 3 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              {selectRepo()}
+              <Dialog
+                open={selectRepoReminder}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                fullWidth
+                maxWidth="sm"
+                style={{ padding: 30 }}
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {'Data Repository not specified'}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    You can create repositories in{' '}
+                    <Button
+                      variant="text"
+                      size="small"
+                      href={`http://146.169.43.78:7200`}
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{ textTransform: 'none' }}
+                    >
+                      GraphDB Workbench
+                    </Button>
+                  </DialogContentText>
+                  {selectRepo()}
+                </DialogContent>
+              </Dialog>
+            </Grid>
+
+            <Grid item xs={12}>
+              Edit the URI prefix of featured database:{' '}
+              <TextField
+                id="outlined-basic"
+                value={db_prefix_URL}
+                size="small"
+                variant="outlined"
+                onChange={(event) => {
+                  dispatch({
+                    type: 'database/setPrefixURL',
+                    payload: event.target.value,
+                  });
+                }}
+                style={{
+                  margin: 3,
+                  backgroundColor: 'white',
+                  width: '100%',
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+
+      {/* Chart */}
+      <Grid item xs={12}>
+        <Paper
+          sx={{
+            height: '100vh',
+          }}
+        >
+          <DataGrid
+            rows={dataRepoTable}
+            columns={columnsRepoTable}
+            onRowClick={(params) => {
+              const title = params.row.title;
+              title && dispatch({ type: 'database/setRepo', payload: title });
+            }}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Repositories;
--- src/pages/dashBoard.tsx
+++ src/pages/dashBoard.tsx
@@ -5,39 +5,32 @@ import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import MenuIcon from '@mui/icons-material/Menu';
 import SchemaIcon from '@mui/icons-material/Schema';
 import {
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  MenuItem,
-  Paper,
-  Select,
-  TextField,
+  Tooltip,
 } from '@mui/material';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
-import MuiDrawer from '@mui/material/Drawer';
-import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
-import { DataGrid } from '@mui/x-data-grid';
-import React, { useEffect, useState } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Repositories from './Repositories';
 import SparqlPage from './SparqlPage';
+import {
+  AppBar,
+  Copyright,
+  Drawer,
+} from './components/DashBoardUtilComponents';
 import GraphsPage from './graphs';
 import { DatabaseState } from './reducer/databaseReducer';
 import SchemaPage from './schemaPage';
-import { getRepoList } from './service';
 
 interface IRepository {
   id: number;
@@ -47,81 +40,13 @@ interface IRepository {
   readable: string;
 }
 
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://www.doc.ic.ac.uk/~yg9418/">
-        Gary Gao
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const drawerWidth: number = 240;
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  '& .MuiDrawer-paper': {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    boxSizing: 'border-box',
-    ...(!open && {
-      overflowX: 'hidden',
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(9),
-      },
-    }),
-  },
-}));
-
 const mdTheme = createTheme();
 
 enum TABS_DASHBOARD {
-  REPOSITORY,
-  SPARQL_QUERY,
-  SCHEMA,
-  GRAPHS,
+  REPOSITORIES = 'Data Repositories',
+  SPARQL_QUERY = 'SPARQL Query',
+  SCHEMA = 'Visualised Schema',
+  GRAPHS = 'Graph Experiments',
 }
 
 function DashboardContent() {
@@ -134,200 +59,16 @@ function DashboardContent() {
 
   const dispatch = useDispatch();
 
-  const [selectRepoReminder, setSelectRepoReminder] = useState<boolean>(false);
-  const [siderOpen, setSiderOpen] = useState(true);
-  const [columnsRepoTable, setColumnsRepoTable] = useState<any>([]);
-  const [dataRepoTable, setDataRepoTable] = useState<any>([]);
-  const [tab, setTab] = useState(TABS_DASHBOARD.REPOSITORY);
-
-  const [repoList, setRepoList] = useState<string[]>([]);
-
-  const data_keys = ['id', 'title', 'uri', 'writable', 'readable'];
-
-  async function fetchRepos() {
-    try {
-      const response = await getRepoList();
-
-      if (response.status === 200) {
-        const data = response.data;
-        const data_bindings = data.results.bindings;
-
-        const columns = [
-          {
-            field: 'title',
-            headerName: 'Title',
-            width: 200,
-          },
-          {
-            field: 'uri',
-            headerName: 'URI',
-            width: 500,
-          },
-          {
-            field: 'writable',
-            headerName: 'Writable',
-            width: 200,
-          },
-          {
-            field: 'readable',
-            headerName: 'Readable',
-            width: 200,
-          },
-        ];
-
-        const dataSource = data_bindings.map(
-          (data_binding: any, index: number) => {
-            return {
-              id: index,
-              title: data_binding.id.value,
-              uri: data_binding.uri.value,
-              writable: data_binding.writable.value,
-              readable: data_binding.readable.value,
-            };
-          },
-        );
-
-        const repoList = data_bindings.map(
-          (data_binding: any, index: number) => {
-            return data_binding.id.value;
-          },
-        );
-
-        setRepoList(['None', ...repoList]);
-        setColumnsRepoTable(columns);
-        setDataRepoTable(dataSource);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    fetchRepos();
-  }, []);
-
-  useEffect(() => {
-    setSelectRepoReminder(
-      repo_graphDB === undefined || repo_graphDB === 'None',
-    );
-  }, [repo_graphDB]);
+  const [siderOpen, setSiderOpen] = useState(false);
+  const [tab, setTab] = useState(TABS_DASHBOARD.REPOSITORIES);
 
   const toggleDrawer = () => {
     setSiderOpen(!siderOpen);
   };
 
-  const selectRepo = () => {
-    return (
-      <Grid>
-        Select a data repository:
-        <Select
-          size="small"
-          value={repo_graphDB ? repoList.indexOf(repo_graphDB) : 0}
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-          style={{
-            margin: 5,
-            backgroundColor:
-              repo_graphDB && repo_graphDB !== 'None' ? '#ccffcc' : '#ffcccc',
-          }}
-        >
-          {repoList.map((repo: any) => {
-            return (
-              <MenuItem
-                key={repoList.indexOf(repo)}
-                value={repoList.indexOf(repo)}
-                onClick={() => {
-                  dispatch({ type: 'database/setRepo', payload: repo });
-                }}
-                style={{
-                  backgroundColor: repo !== 'None' ? '#ccffcc' : '#ffcccc',
-                  borderRadius: 5,
-                  border: '1px solid #aaa',
-                  margin: 3,
-                }}
-              >
-                {repo}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </Grid>
-    );
-  };
-
   function tabContents(tab_current: TABS_DASHBOARD) {
-    if (tab_current === TABS_DASHBOARD.REPOSITORY) {
-      return (
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper sx={{ padding: 3 }}>
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  {selectRepo()}
-                  <Dialog
-                    open={selectRepoReminder}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                    fullWidth
-                    maxWidth="sm"
-                    style={{ padding: 30 }}
-                  >
-                    <DialogTitle id="alert-dialog-title">
-                      {'Data Repository not specified'}
-                    </DialogTitle>
-                    <DialogContent>
-                      <DialogContentText id="alert-dialog-description">
-                        You have not selected a data repository!
-                      </DialogContentText>
-                      {selectRepo()}
-                    </DialogContent>
-                  </Dialog>
-                </Grid>
-
-                <Grid item xs={12}>
-                  Edit the URI prefix of featured database:{' '}
-                  <TextField
-                    id="outlined-basic"
-                    value={db_prefix_URL}
-                    size="small"
-                    variant="outlined"
-                    onChange={(event) => {
-                      dispatch({
-                        type: 'database/setPrefixURL',
-                        payload: event.target.value,
-                      });
-                    }}
-                    style={{
-                      margin: 3,
-                      backgroundColor: 'white',
-                      width: '100%',
-                    }}
-                  />
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-
-          {/* Chart */}
-          <Grid item xs={12}>
-            <Paper
-              sx={{
-                height: '100vh',
-              }}
-            >
-              <DataGrid
-                rows={dataRepoTable}
-                columns={columnsRepoTable}
-                onRowClick={(params) => {
-                  const title = params.row.title;
-                  title &&
-                    dispatch({ type: 'database/setRepo', payload: title });
-                }}
-              />
-            </Paper>
-          </Grid>
-        </Grid>
-      );
+    if (tab_current === TABS_DASHBOARD.REPOSITORIES) {
+      return <Repositories />;
     }
     if (tab_current === TABS_DASHBOARD.SPARQL_QUERY) {
       return (
@@ -344,6 +85,20 @@ function DashboardContent() {
     }
     return 'ERROR';
   }
+
+  const sider_tabs = [
+    TABS_DASHBOARD.REPOSITORIES,
+    TABS_DASHBOARD.SPARQL_QUERY,
+    TABS_DASHBOARD.SCHEMA,
+    TABS_DASHBOARD.GRAPHS,
+  ];
+
+  const sider_icon_map = {
+    [TABS_DASHBOARD.REPOSITORIES]: <DashboardIcon />,
+    [TABS_DASHBOARD.SPARQL_QUERY]: <ManageSearchIcon />,
+    [TABS_DASHBOARD.SCHEMA]: <SchemaIcon />,
+    [TABS_DASHBOARD.GRAPHS]: <AssessmentIcon />,
+  };
 
   return (
     <ThemeProvider theme={mdTheme}>
@@ -400,53 +155,21 @@ function DashboardContent() {
           <Divider />
           <List component="nav">
             <React.Fragment>
-              <ListItemButton
-                selected={tab === TABS_DASHBOARD.REPOSITORY}
-                onClick={() => {
-                  setTab(TABS_DASHBOARD.REPOSITORY);
-                }}
-              >
-                <ListItemIcon>
-                  <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText primary="Dashboard" />
-              </ListItemButton>
-
-              <ListItemButton
-                selected={tab === TABS_DASHBOARD.SPARQL_QUERY}
-                onClick={() => {
-                  setTab(TABS_DASHBOARD.SPARQL_QUERY);
-                }}
-              >
-                <ListItemIcon>
-                  <ManageSearchIcon />
-                </ListItemIcon>
-                <ListItemText primary="SPARQL Query" />
-              </ListItemButton>
-
-              <ListItemButton
-                selected={tab === TABS_DASHBOARD.SCHEMA}
-                onClick={() => {
-                  setTab(TABS_DASHBOARD.SCHEMA);
-                }}
-              >
-                <ListItemIcon>
-                  <SchemaIcon />
-                </ListItemIcon>
-                <ListItemText primary="View Schema" />
-              </ListItemButton>
-
-              <ListItemButton
-                selected={tab === TABS_DASHBOARD.GRAPHS}
-                onClick={() => {
-                  setTab(TABS_DASHBOARD.GRAPHS);
-                }}
-              >
-                <ListItemIcon>
-                  <AssessmentIcon />
-                </ListItemIcon>
-                <ListItemText primary="Graph Experiments" />
-              </ListItemButton>
+              {sider_tabs.map((t: TABS_DASHBOARD) => {
+                return (
+                  <Tooltip title={t} placement="right">
+                    <ListItemButton
+                      selected={tab === t}
+                      onClick={() => {
+                        setTab(t);
+                      }}
+                    >
+                      <ListItemIcon>{sider_icon_map[t]}</ListItemIcon>
+                      <ListItemText primary={t} />
+                    </ListItemButton>
+                  </Tooltip>
+                );
+              })}
             </React.Fragment>
             <Divider sx={{ my: 1 }} />
             {/* {secondaryListItems} */}
