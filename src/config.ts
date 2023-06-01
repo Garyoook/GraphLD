f94132bb3c76becce9eb5ff7ae2d70fe7c92b931
Gary Gao <yg9418@ic.ac.uk>
2023-06-01 11:24:23 +0100

    added auto switching graphDB host based on node env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
-export const GRAPHDB_HOST = 'http://127.0.0.1:7200';
-// export const GRAPHDB_HOST = 'http://146.169.43.78:7200';
+const node_env = process.env.NODE_ENV;
+// export const GRAPHDB_HOST = 'http://127.0.0.1:7200';
+export const GRAPHDB_HOST =
+  node_env === 'production'
+    ? 'http://146.169.43.78:7200'
+    : 'http://127.0.0.1:7200';
 
 export const GraphDB_config = {
   repo_graphDB: 'SemanticWebVis',
