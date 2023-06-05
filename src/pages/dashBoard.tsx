073fb8b93895b76030d4f6e8eb2fa5ae976cfc73
Gary Gao <yg9418@ic.ac.uk>
2023-06-05 11:07:01 +0100

    optimiesd tooltip in dashboard sider bar

--- src/pages/dashBoard.tsx
+++ src/pages/dashBoard.tsx
@@ -155,7 +155,7 @@ function DashboardContent() {
             <React.Fragment>
               {sider_tabs.map((t: TABS_DASHBOARD) => {
                 return (
-                  <Tooltip key={t} title={t} placement="right">
+                  <Tooltip key={t} title={siderOpen ? '' : t} placement="right">
                     <ListItemButton
                       selected={tab === t}
                       onClick={() => {
