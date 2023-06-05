842835625f3fe350f3ef5d4d9d8590cec41bb6fe
Gary Gao <yg9418@ic.ac.uk>
2023-06-05 14:08:04 +0100

    added autocompletions for SPARQL related syntaxes

--- src/pages/SparqlPage/codeMirrorConfigs.ts
+++ src/pages/SparqlPage/codeMirrorConfigs.ts
@@ -1,5 +1,101 @@
 import { EditorView } from 'codemirror';
 
+const SPARQLSyntax_basics = [
+  // using info: 'Basic syntaxes of SPARQL'
+  // query forms:
+  'SELECT',
+  'CONSTRUCT',
+  'ASK',
+  'DESCRIBE',
+  // solution sequence & modifiers:
+  'ORDER BY',
+  'DISTINCT',
+  'REDUCED',
+  'OFFSET',
+  'LIMIT',
+  // aggregation:
+  'GROUP BY',
+  'HAVING',
+  // optoinal values:
+  'OPTIONAL',
+  // filter:
+  'FILTER',
+];
+
+const SPARQLSyntax_aggregates = [
+  // using info: 'Aggregates in SPARQL'
+  // aggregate algebra:
+  'COUNT',
+  'SUM',
+  'AVG',
+  'MIN',
+  'MAX',
+  'GroupConcat',
+  'SAMPLE',
+];
+
+const SPARQLSyntax_functions = [
+  // using info: 'Functions in SPARQL'
+  // Function Definitions:
+  'BOUND()',
+  'IF()',
+  'COALESCE()',
+  'NOT EXISTS()',
+  'EXISTS()',
+  'sameTerm()',
+  'IN()',
+  'NOT IN()',
+  // Functions on RDF Terms: // isIRI, isBlank, isLiteral, isNumeric, str, lang, datatype, IRI, BNODE, STRDT, STRLANG, UUID, STRUUID
+  'isIRI()',
+  'isBlank()',
+  'isLiteral()',
+  'isNumeric()',
+  'str()',
+  'lang()',
+  'datatype()',
+  'IRI()',
+  'BNODE()',
+  'STRDT()',
+  'STRLANG()',
+  'UUID()',
+  'STRUUID()',
+  // Functions on strings: // STRLEN, SUBSTR, UCASE LCASE, STRSTARTS, STRENDS, CONTAINS, STRBEFORE STRAFTER, ENCODE_FOR_URI, CONCAT, langMatches, REGEX REPLACE
+  'STRLEN()',
+  'SUBSTR()',
+  'UCASE()',
+  'LCASE()',
+  'STRSTARTS()',
+  'STRENDS()',
+  'CONTAINS()',
+  'STRBEFORE()',
+  'STRAFTER()',
+  'ENCODE_FOR_URI()',
+  'CONCAT()',
+  'langMatches()',
+  'REGEX()',
+  'REPLACE()',
+  // Functions on numerics: // abs, round, ceil, floor, RAND
+  'abs()',
+  'round()',
+  'ceil()',
+  'floor()',
+  'RAND()',
+];
+
+const SPARQL_syntax_with_info = [
+  // logical-or, logical-and,
+  {
+    label: '||',
+    type: 'sparql',
+    info: 'logical or in SPARQL',
+  },
+  {
+    label: '&&',
+    type: 'sparql',
+    info: 'logical and in SPARQL',
+  },
+];
+
 const owlClasses = [
   'owl:AllDifferent',
   'owl:AnnotationProperty',
@@ -115,6 +211,29 @@ export const defaultAutocompletions = [
     type: 'owl',
     boost: 1,
   },
+  // SPAQRL syntaxes
+  ...SPARQLSyntax_basics.map((item) => {
+    return {
+      label: item,
+      type: 'sparql',
+      info: 'Basic syntaxes of SPARQL',
+    };
+  }),
+  ...SPARQLSyntax_aggregates.map((item) => {
+    return {
+      label: item,
+      type: 'sparql',
+      info: 'Aggregates in SPARQL',
+    };
+  }),
+  ...SPARQLSyntax_functions.map((item) => {
+    return {
+      label: item,
+      type: 'sparql',
+      info: 'Functions in SPARQL',
+    };
+  }),
+  ...SPARQL_syntax_with_info,
 ];
 
 export const customIconsTheme = /*@__PURE__*/ EditorView.theme({
@@ -145,6 +264,14 @@ export const customIconsTheme = /*@__PURE__*/ EditorView.theme({
   '.cm-completionIcon-rdfs': {
     '&:after': {
       content: "'RDFS'",
+      fontSize: '50%',
+      verticalAlign: 'middle',
+    },
+  },
+
+  '.cm-completionIcon-sparql': {
+    '&:after': {
+      content: "'SPARQL'",
       fontSize: '50%',
       verticalAlign: 'middle',
     },
