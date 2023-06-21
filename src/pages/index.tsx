be2f882a1a7ab6ffe20b9761d9f5b0ec2927df8f
Gary Gao <yg9418@ic.ac.uk>
2023-06-22 00:20:44 +0100

    optimise the initial full screen loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,68 +26,73 @@ function HomePage() {
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 500);
+    }, 1000);
   }, []);
+
+  function FullScreenLoading() {
+    return (
+      <Backdrop
+        sx={{
+          color: '#fff',
+          fontSize: 60,
+          fontWeight: 'bold',
+          backgroundColor: '#1976d2',
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          fontFamily: '"Gill Sans", sans-serif',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+        open={loading}
+        TransitionComponent={Grow}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            margin: 30,
+          }}
+        >
+          <div
+            style={{
+              marginRight: 20,
+              alignSelf: 'flex-end',
+            }}
+          >
+            GraphLD
+          </div>
+
+          <div
+            style={{
+              color: '#fff',
+              fontSize: 30,
+              alignSelf: 'flex-end',
+            }}
+          >
+            - better way to know your Linked Data
+          </div>
+        </div>
+
+        <Box
+          sx={{
+            width: '60%',
+          }}
+        >
+          <LinearProgress color="inherit" />
+        </Box>
+      </Backdrop>
+    );
+  }
 
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
           {/* {RDFPage()} */}
-          {loading ? (
-            <Backdrop
-              sx={{
-                color: '#fff',
-                fontSize: 60,
-                fontWeight: 'bold',
-                backgroundColor: '#1976d2',
-                zIndex: (theme) => theme.zIndex.drawer + 1,
-                fontFamily: '"Gill Sans", sans-serif',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-              }}
-              open={loading}
-              TransitionComponent={Grow}
-            >
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  margin: 30,
-                }}
-              >
-                <div
-                  style={{
-                    marginRight: 20,
-                    alignSelf: 'flex-end',
-                  }}
-                >
-                  GraphLD
-                </div>
-
-                <div
-                  style={{
-                    color: '#fff',
-                    fontSize: 30,
-                    alignSelf: 'flex-end',
-                  }}
-                >
-                  - better way to know your Linked Data
-                </div>
-              </div>
-
-              <Box
-                sx={{
-                  width: '60%',
-                }}
-              >
-                <LinearProgress color="inherit" />
-              </Box>
-            </Backdrop>
-          ) : (
-            Dashboard()
-          )}
+          {loading ? FullScreenLoading() : ''}
+          <div style={{ display: loading ? 'none' : 'block' }}>
+            {Dashboard()}
+          </div>
         </header>
       </div>
     </Provider>
