96979c00f42b3218465a48c71b4ad4c930884417
Gary Gao <yg9418@ic.ac.uk>
2023-06-28 20:30:38 +0100

    changed graphDB repo to doc website

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 const node_env = process.env.NODE_ENV;
-// export const GRAPHDB_HOST = 'http://127.0.0.1:7200';
 
+// const GRAPHDB_HOST_DEV = 'http://127.0.0.1:7200';
+// const GRAPHDB_HOST_PROD = 'http://146.169.43.78:7200';
 const GRAPHDB_HOST_DEV = 'http://127.0.0.1:7200';
-const GRAPHDB_HOST_PROD = 'http://146.169.43.78:7200';
+const GRAPHDB_HOST_PROD = 'http://graphdb.doc.ic.ac.uk';
 
 export const GRAPHDB_HOST =
   node_env === 'production' ? GRAPHDB_HOST_PROD : GRAPHDB_HOST_DEV;
